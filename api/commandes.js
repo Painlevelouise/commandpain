32b204d11bebe822cb055d483f08d030569f929e
Painlevelouise <julien.faerdig@gmail.com>
2025-01-17 15:48:03 +0100

    Update commandes.js

--- api/commandes.js
+++ api/commandes.js
@@ -1,9 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
 export default (req, res) => {
+    const filePath = path.join(process.cwd(), 'data', 'commandes.json');
+
     if (req.method === 'POST') {
         const nouvelleCommande = req.body;
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Erreur de lecture:', err);
+                return res.status(500).json({ message: 'Erreur interne du serveur' });
+            }
 
-        // Logique pour traiter et enregistrer la commande
-        res.status(201).json({ message: 'Commande enregistrée avec succès' });
+            const commandes = JSON.parse(data || '[]');
+            commandes.push(nouvelleCommande);
+
+            fs.writeFile(filePath, JSON.stringify(commandes, null, 2), 'utf8', (err) => {
+                if (err) {
+                    console.error('Erreur d\'écriture:', err);
+                    return res.status(500).json({ message: 'Erreur interne du serveur' });
+                }
+                res.status(201).json({ message: 'Commande enregistrée avec succès' });
+            });
+        });
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Méthode ${req.method} non autorisée`);
